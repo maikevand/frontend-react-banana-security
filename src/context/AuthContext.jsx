ee84e4ed155ec6676f2c9f30d07d74879de2ce9b
maike1983 <Maikevandartel@outlook.com>
2026-06-09 16:58:33 +0200

    t/m stap 10, button roept log-in context aan

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,15 +1,26 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
+import { useNavigate} from "react-router-dom";
 
 export const AuthContext = createContext(null);
 
 function AuthContextProvider({children}) {
-const authData = {
-isAuth: true,
-    user: "M"
-};
+    const [isAuth, setIsAuth] = useState(false);
+    const navigate = useNavigate();
+
+    function login() {
+        setIsAuth(true);
+        console.log("Gebruiker is ingelogd!");
+        navigate("/profile");
+    }
+
+    const authData = {
+        isAuth,
+        user: "M",
+        login
+    };
     return (
         <AuthContext.Provider value={authData}>
-            { children }
+            {children}
         </AuthContext.Provider>
     )
 }
